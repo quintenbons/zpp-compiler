0b1a363c06e68e30f0de18310613e546900931ff
Quinten Bons <QuintenBons@outlook.com>
2024-11-21 21:38:53 +0000

    Improve build process

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-BUILD_PATH=./cmake-build
+#!/bin/bash
+SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+BUILD_PATH="$SCRIPT_PATH/cmake-build"
 
 if [ "$1" == "-c" ]; then
   echo "== Cleaning full $BUILD_PATH =="
@@ -11,9 +12,14 @@ mkdir -p $BUILD_PATH && cd $BUILD_PATH
 
 if [ "$1" == "-d" ]; then
   shift
-  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Debug $SCRIPTPATH
+  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Debug $SCRIPT_PATH
 else
-  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Release $SCRIPTPATH
+  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Release $SCRIPT_PATH
 fi
 
 make $@
+
+if [ -f "$BUILD_PATH/compile_commands.json" ]; then
+  echo "Generated compile_commands.json (for code navigation) symlink next to build script"
+  ln -sf "$BUILD_PATH/compile_commands.json" $SCRIPT_PATH/compile_commands.json
+fi
