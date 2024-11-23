1970afba449123ae7ad4e50ef1afc556f203cf53
Quinten Bons <QuintenBons@outlook.com>
2024-11-23 20:33:42 +0000

    Rebranding

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-BUILD_PATH="$SCRIPT_PATH/cmake-build"
+if [ -z $ZPP_REPO_PATH ] ; then
+  source $SCRIPT_PATH/setenv.sh
+fi
 
 if [ "$1" == "-c" ]; then
-  echo "== Cleaning full $BUILD_PATH =="
-  rm -rf $BUILD_PATH
+  echo "== Cleaning full $ZPP_BUILD_PATH =="
+  rm -rf $ZPP_BUILD_PATH
   shift
 fi
 
-mkdir -p $BUILD_PATH && cd $BUILD_PATH
+mkdir -p $ZPP_BUILD_PATH && cd $ZPP_BUILD_PATH
 
 if [ "$1" == "-d" ]; then
   shift
-  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Debug $SCRIPT_PATH
+  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Debug $ZPP_REPO_PATH
 else
-  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Release $SCRIPT_PATH
+  LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake -DCMAKE_BUILD_TYPE=Release $ZPP_REPO_PATH
 fi
 
 make $@
 
-if [ -f "$BUILD_PATH/compile_commands.json" ]; then
+if [ -f "$ZPP_BUILD_PATH/compile_commands.json" ]; then
   echo "Generated compile_commands.json (for code navigation) symlink next to build script"
-  ln -sf "$BUILD_PATH/compile_commands.json" $SCRIPT_PATH/compile_commands.json
+  ln -sf "$ZPP_BUILD_PATH/compile_commands.json" $ZPP_REPO_PATH/compile_commands.json
 fi
