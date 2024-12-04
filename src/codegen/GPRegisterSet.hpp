6ea7d6f20c6f24d8c806a3113587bf4e0bd7db1a
Quinten Bons <QuintenBons@outlook.com>
2024-12-04 22:04:35 +0000

    General purpose register set (allocator)

---
+++ src/codegen/GPRegisterSet.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <bitset>
+
+#include "ast/scopes/registers.hpp"
+#include "dbg/errors.hpp"
+
+namespace scopes
+{
+
+class GPRegisterSet {
+public:
+  static constexpr size_t GP_REGISTER_COUNT = static_cast<size_t>(GeneralPurposeRegister::_COUNT);
+
+public:
+  std::optional<Register> acquire() {
+    for (size_t i = 0; i < takenRegisters.size(); ++i) {
+        if (!takenRegisters.test(i)) {
+          takenRegisters.set(i);
+          return static_cast<Register>(i);
+        }
+    }
+    return std::nullopt;
+  };
+
+  void release(Register reg) {
+    size_t idx = static_cast<size_t>(reg);
+    DEBUG_ASSERT(takenRegisters.test(idx), "Tried to release general purpose register that was not acquired (double release?)");
+    takenRegisters.reset();
+  }
+
+  const std::bitset<GP_REGISTER_COUNT> &asBistet() const { return takenRegisters; }
+
+protected:
+  size_t findFirstUnSet() {
+  }
+
+private:
+  std::bitset<GP_REGISTER_COUNT> takenRegisters;
+};
+
+
+
+} /* namespace scopes */
